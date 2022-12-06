5e262bc6b6d54b4aaf95e16f53bd6a14146d9320
ashley <ashley.oketo@student.moringaschool.com>
2022-12-06 17:10:09 +0300

    Generated the user,quote and comment models and included there relationships

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,7 @@
 class Quote < ApplicationRecord
-    has_many :categories
     has_many :comments
     has_many :users, through: :comments
+
+    validates :description, presence: true
+    validates :description,length:{ minimum:20 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
     has_many :comments
     has_many :quotes, through: :comments
+    has_secure_password
 
-    # validates :name, presence: true
-    # validates :name, uniqueness: true
+     validates :name, presence: true
+     validates :name, uniqueness: true
 end
