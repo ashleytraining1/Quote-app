3255a6e8388d3950e2dd1d5f0d941a722c5100d3
ashley <ashley.oketo@student.moringaschool.com>
2022-12-09 13:16:54 +0300

    made changes in the user and quote models

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,6 @@
 class Quote < ApplicationRecord
     has_many :comments
-    has_many :users, through: :comments
+    has_many :users
 
     validates :description, presence: true
     validates :description,length:{ minimum:20 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :comments
-    has_many :quotes, through: :comments
+    has_many :comments, through: :quotes
     has_secure_password
 
      validates :name, presence: true
